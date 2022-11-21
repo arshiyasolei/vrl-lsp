1040c51d2c04265fc9cc3f5338c4df7c9ed28ec2
Arshia Soleimani <arshia.soleimani@datadoghq.com>
2022-11-21 11:36:30 -0500

    remove the need for channels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use codespan_lsp::byte_index_to_position;
-use crossbeam_channel::{Receiver, Sender};
 use std::fmt::Debug;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
@@ -9,8 +8,6 @@ use vrl::diagnostic::DiagnosticList;
 #[derive(Debug)]
 struct Backend {
     client: Client,
-    tx_text: Sender<String>,
-    rx_diag: Receiver<Option<DiagnosticList>>,
 }
 
 impl Backend {
@@ -85,8 +82,16 @@ impl LanguageServer for Backend {
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
         let txt = params.content_changes[0].text.clone();
-        self.tx_text.send(txt.clone()).unwrap();
-        if let Some(diagonstics) = self.rx_diag.recv().unwrap() {
+        let diagnostics = {
+            let fns = vrl_stdlib::all();
+            let res = vrl::compile(txt.as_str(), &fns);
+            if let Err(e) = res {
+                Some(e)
+            } else {
+                None
+            }
+        };
+        if let Some(diagonstics) = diagnostics {
             self.process_diagnostics(params.text_document.uri.clone(), txt, diagonstics)
                 .await;
         } else {
@@ -99,8 +104,16 @@ impl LanguageServer for Backend {
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
         let txt = params.text_document.text.clone();
-        self.tx_text.send(txt.clone()).unwrap();
-        if let Some(diagonstics) = self.rx_diag.recv().unwrap() {
+        let diagnostics = {
+            let fns = vrl_stdlib::all();
+            let res = vrl::compile(txt.as_str(), &fns);
+            if let Err(e) = res {
+                Some(e)
+            } else {
+                None
+            }
+        };
+        if let Some(diagonstics) = diagnostics {
             self.process_diagnostics(params.text_document.uri.clone(), txt, diagonstics)
                 .await;
         } else {
@@ -126,23 +139,6 @@ impl LanguageServer for Backend {
 async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
-    // Handle compilation in a separate thread due to the compile result not being `Send`.
-    let (tx_text, rx_text) = crossbeam_channel::unbounded();
-    let (tx_diag, rx_diag) = crossbeam_channel::unbounded();
-    let fns = vrl_stdlib::all();
-    std::thread::spawn(move || loop {
-        let full_text: String = rx_text.recv().unwrap();
-        let res = vrl::compile(full_text.as_str(), &fns);
-        if let Err(e) = res {
-            tx_diag.send(Some(e)).unwrap();
-        } else {
-            tx_diag.send(None).unwrap();
-        }
-    });
-    let (service, socket) = LspService::new(|client| Backend {
-        client,
-        tx_text,
-        rx_diag,
-    });
+    let (service, socket) = LspService::new(|client| Backend { client });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
